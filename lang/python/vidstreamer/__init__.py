56996795d228dc03a8bb02a58580ceb29bf2f22c
Nick <nicholas.c.burnett@gmail.com>
2020-06-03 12:27:10 -0400

    Adding render output function and server

--- lang/python/vidstreamer/__init__.py
+++ lang/python/vidstreamer/__init__.py
@@ -6,7 +6,17 @@ import os
 import sys
 import time
 
+from flask import Flask, jsonify, request, Response
 from . import analytic_pb2
+
+class EndpointAction(object):
+
+    def __init__(self, action):
+        self.action = action
+
+    def __call__(self, *args):
+        answer = self.action()
+        return answer
 
 def default_output_func(frame, req, resp):
     output = [req.frame_num]
@@ -22,10 +32,51 @@ def default_output_func(frame, req, resp):
         output.append(roi.confidence)
     print(outstring.format(*output))
 
+def render(frame, req, resp, window_name="Output"):
+    for roi in resp.roi:
+        if roi.HasField("box"):
+            box = roi.box
+            display_text = "{!s} - {!s}".format(roi.classification, roi.confidence)
+            cv2.rectangle(frame, (box.corner1.x, box.corner1.y), (box.corner2.x, box.corner2.y), (255, 0, 0), 2)
+            cv2.putText(frame, display_text, (box.corner1.x, box.corner1.y), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
+    cv2.imshow(window_name, frame)
+    cv2.waitKey(1)
+
+class AnalyticServer:
+    def __init__(self, name, host="::", port=50051):
+        self.app = Flask(name)
+        self.host = host
+        self.port = port
+        self.func = None
+        self.add_endpoint("/process", "process", self.process, methods=["POST"])
+        
+    def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None, methods=None):
+        self.app.add_url_rule(endpoint, endpoint_name, EndpointAction(handler), methods=methods)
+
+    def run(self):
+        logging.info("Server running on {!s}:{!s}".format(self.host, self.port))
+        self.app.run(host=self.host, port=self.port)
+
+    def register_process_func(self, func):
+        self.process_func = func
+
+    def register_output_func(self, output_func):
+        self.output_func = output_func
+
+    def process(self, req, resp):
+        frame = cv2.imdecode(np.fromstring(req.frame.img, dtype=np.uint8), 1)
+        self.process_func(frame, req, resp)
+        if self.output_func:
+            self.output_func(frame, req, resp)
+
+
 class Streamer:
-    def __init__(self, func=None):
+    def __init__(self, func=None, output_func="default"):
         self.analytic_func = func
         self.output_func = default_output_func
+        if output_func == "render":
+            self.output_func = render
+        
 
     def check_func(self):
         if not self.analytic_func:
@@ -77,6 +128,13 @@ class Streamer:
             self.output_func(frame, req, resp)
         return req, resp
         
+    def serve(self, port=50051):
+        analytic_server = AnalyticServer(name=__name__, port=port)
+        analytic_server.register_process_func(self.process_frame)
+        analytic_server.register_output_func(self.output_func)
+        analytic_server.run()  
+
+
     def run(self):
         """ The run function starts a process to send image/video data to the analtyic. Arguments can
         be used to specify a connected camera, video file, or image file to be processed, or used to
@@ -86,15 +144,20 @@ class Streamer:
         parser.add_argument("--videofile", default=None, help="Video file to process")
         parser.add_argument("--imagefile", default=None, help="Image file to process")
         parser.add_argument("--camera_id", default=0, help="Camera ID on the host machine")
+        parser.add_argument("--serve", default=False, help="If true starts up an analytic service", action="store_true")
+        parser.add_argument("--service_port", default=50051, help="Port on which to run the analytic service")
         args = parser.parse_args()
 
-        if args.videofile:
-            self.stream_video(args.videofile)
-        elif args.imagefile:
-            self.stream_image(args.imagefile)
-        elif args.camera_id:
-            self.stream_camera(args.camera_id)
+        if args.serve:
+            self.serve(port=args.service_port)
         else:
-            raise ValueError("No Valid option specified. Must specify a source (Video, Image, or Camera")
+            if args.videofile:
+                self.stream_video(args.videofile)
+            elif args.imagefile:
+                self.stream_image(args.imagefile)
+            elif args.camera_id:
+                self.stream_camera(args.camera_id)
+            else:
+                raise ValueError("No Valid option specified. Must specify a source (Video, Image, or Camera")
 
 
